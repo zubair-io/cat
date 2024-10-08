e60f5e361e93f7d61d352014f63bebbb18a407c4
stcalica <kycalica@gmail.com>
2024-10-08 15:02:50 +0530

    updates to openapi to collection

--- actions/transform-openapi/index.js
+++ actions/transform-openapi/index.js
@@ -1,14 +1,18 @@
 const core = require('@actions/core');
 const axios = require('axios');
+//TODO: add ability to go from Postman collection to OpenAPI schema
+//const { transpile } = require("postman2openapi");
 
 async function run() {
   try {
     const apiKey = core.getInput('postman_api_key');
     const openapiSchema = core.getInput('openapi_schema');
+    const openapi = transpile(collection);
 
-    const response = await axios.post(`https://api.getpostman.com/apis/schemas`, {
+    //TODO: pass through options and allow for workspace to be passed in as URL parameter
+    const response = await axios.post(`https://api.getpostman.com/import/openapi`, {
       type: "openapi:3",
-      files: [{ path: "openapi.json", content: openapiSchema }]
+      input: openapiSchema
     }, {
       headers: { 'X-Api-Key': apiKey, 'Content-Type': 'application/json' }
     });
