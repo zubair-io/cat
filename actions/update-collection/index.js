2d9c3c0c521118b6a8891a4473595b3f7261180c
Kyle Calica <stcalica@users.noreply.github.com>
2024-10-10 09:55:04 +0530

    Update index.js

--- actions/update-collection/index.js
+++ actions/update-collection/index.js
@@ -4,9 +4,9 @@ const axios = require('axios');
 async function run() {
   try {
     const apiKey = core.getInput('postman_api_key');
-    const destinationCollectionId = core.getInput('destination_collection_id');
+    const updateCollectionId = core.getInput('update_collection_id');
     const collectionData = core.getInput('collection_data');
-    removeIds(destinationCollectionId);
+    removeIds(updateCollectionId);
 
 
     const response = await axios.put(`https://api.getpostman.com/collections/${destinationCollectionId}`, { collection: collectionData }, {
